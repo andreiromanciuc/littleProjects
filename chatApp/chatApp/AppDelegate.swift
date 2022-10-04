c3d05f6cfa84f85e4730762efad8b94c86911d2f
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2022-10-04 10:09:46 +0300

    added IQPackageKeyboardManager to the project

--- chatApp/chatApp/AppDelegate.swift
+++ chatApp/chatApp/AppDelegate.swift
@@ -7,16 +7,19 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
         
         let db = Firestore.firestore()
-        print(db)
+        IQKeyboardManager.shared.enable = true
         
         return true
     }
