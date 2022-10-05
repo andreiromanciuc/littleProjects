5c39083724e3440e080a50202ff8824a07ad9d97
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2022-10-05 11:09:31 +0300

    added new personalCard SwiftUI project

---
+++ personalCard/personalCard/personalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalCardApp.swift
+//  personalCard
+//
+//  Created by Andrei Romanciuc on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct personalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
