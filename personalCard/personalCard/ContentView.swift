b83ad537607c92e5e757a9db9d65c82e36e17446
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2022-10-05 14:27:18 +0300

    finished personalCard app

--- personalCard/personalCard/ContentView.swift
+++ personalCard/personalCard/ContentView.swift
@@ -9,18 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 0.20, green: 0.67, blue: 0.88)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("Andrei")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 6)
+                    )
+
+                Text("Andrei Romanciuc")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+                
+                Divider()
+                
+                InfoView(text: "+40 752 465 671", image: "phone.fill")
+                
+                InfoView(text: "andrei.romanciuc@gmail.com", image: "envelope.fill")
+                
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    
+    let text: String
+    let image: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(
+                HStack{
+                    Image(systemName: image)
+                        .foregroundColor(Color(red: 0.20, green: 0.67, blue: 0.88))
+                    Text(text).foregroundColor(.black)
+                    
+                })
     }
 }
