84ab414df3cd3d65c9312d6ccd8f319592cf9d08
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2022-10-05 18:17:57 +0300

    used The Observer Design Pattern
    in this case was used mentioned design pattern to get data from api and to fetch it to posts list

--- H4ckerNews/H4ckerNews/Models/NetworkManager.swift
+++ H4ckerNews/H4ckerNews/Models/NetworkManager.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-class NetworkManager {
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
     
     func fetchData(){
         if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page"){
@@ -18,6 +20,8 @@ class NetworkManager {
                     if let safeData = data {
                         do {
                             let results = try decoder.decode(Results.self, from: safeData)
+                            self.posts = results.hits
+                            
                         } catch {
                             print(error)
                         }
