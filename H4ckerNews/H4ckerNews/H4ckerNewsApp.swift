59549cd0823b6d88df247cb0e6ef62b3db4d2646
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2022-10-05 16:20:26 +0300

    started hanker news app

---
+++ H4ckerNews/H4ckerNews/H4ckerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4ckerNewsApp.swift
+//  H4ckerNews
+//
+//  Created by Andrei Romanciuc on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4ckerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
