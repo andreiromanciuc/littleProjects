6cbe246f26b0b534350885ce2db01d2be706cc74
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2022-10-25 17:05:29 +0300

    made table view storyboard

---
+++ ToDoList/ToDoList/ToDoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Andrei Romanciuc on 25.10.2022.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    
+}
+
