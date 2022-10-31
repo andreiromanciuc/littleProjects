f980cadf0f10f41ec06a5e418a438a00834741c7
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2022-10-31 11:19:37 +0200

    resolved error on thread 1

--- ToDoList/ToDoList/Controllers/CategoryViewController.swift
+++ ToDoList/ToDoList/Controllers/CategoryViewController.swift
@@ -17,7 +17,7 @@ class CategoryViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //loadLists()
+        loadLists()
     }
 
     
@@ -38,57 +38,57 @@ class CategoryViewController: UITableViewController {
     
     //MARK: - Data manipulation Methods
     
-//    func saveList() {
-//
-//        do {
-//            try context.save()
-//        } catch {
-//            print("Error saving context, \(error)")
-//        }
-//
-//        tableView.reloadData()
-//    }
+    func saveList() {
+
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context, \(error)")
+        }
+
+        tableView.reloadData()
+    }
     
     
-//    func loadLists(){
-//        let request: NSFetchRequest<Category> = Category.fetchRequest()
-//        do{
-//           categoryArray = try context.fetch(request)
-//        } catch {
-//            print("Error fetching data from context \(error)")
-//        }
-//
-//        tableView.reloadData()
-//    }
+    func loadLists(){
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        do{
+           categoryArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+
+        tableView.reloadData()
+    }
 
     
     //MARK: - Add New Categories
     
-//    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-//
-//        var textField = UITextField()
-//
-//        let alert = UIAlertController(title: "Add New List", message: "", preferredStyle: .alert)
-//        let action = UIAlertAction(title: "Add list", style: .default) { (action) in
-//
-//        let category = Category(context: self.context)
-//
-//            if let text = textField.text {
-//                category.name = text
-//                self.categoryArray.append(category)
-//            }
-//
-//            self.saveList()
-//        }
-//
-//        alert.addTextField {(alertTextField) in
-//            alertTextField.placeholder = "Add new list"
-//            textField = alertTextField
-//        }
-//
-//        alert.addAction(action)
-//
-//        present(alert, animated: true)
-//    }
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+
+        var textField = UITextField()
+
+        let alert = UIAlertController(title: "Add New List", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add list", style: .default) { (action) in
+
+        let category = Category(context: self.context)
+
+            if let text = textField.text {
+                category.name = text
+                self.categoryArray.append(category)
+            }
+
+            self.saveList()
+        }
+
+        alert.addTextField {(alertTextField) in
+            alertTextField.placeholder = "Add new list"
+            textField = alertTextField
+        }
+
+        alert.addAction(action)
+
+        present(alert, animated: true)
+    }
 }
 
